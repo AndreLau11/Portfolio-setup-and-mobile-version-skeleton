1422714cf850915e52bc203b63027f7263dc4d37
AndreLau11 <hectorlau.1993@gmail.com>
2022-02-28 13:00:04 -0600

    Add the linter corrections

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const navOpen = document.getElementById('navbar-hamburger');
-const mobileNav = document.getElementById('mobile-nav')
-const navClose = document.getElementById('close-menu')
-
-const navPortfolio = document.getElementById("mobile__nav_portfolio")
-const navAbout = document.getElementById("mobile__nav_about")
-const navContact = document.getElementById("mobile__nav_contact")
+const mobileNav = document.getElementById('mobile-nav');
+const navClose = document.getElementById('close-menu');
+const navPortfolio = document.getElementById('mobile__nav_portfolio');
+const navAbout = document.getElementById('mobile__nav_about');
+const navContact = document.getElementById('mobile__nav_contact');
 
 function showNavbar() {
-    mobileNav.classList.remove('hidden');
-    navOpen.classList.add('hidden');
+  mobileNav.classList.remove('hidden');
+  navOpen.classList.add('hidden');
 }
 
 function hideNavbar() {
-    mobileNav.classList.add('hidden');
-    navOpen.classList.remove('hidden');
+  mobileNav.classList.add('hidden');
+  navOpen.classList.remove('hidden');
 }
 
 navOpen.addEventListener('click', showNavbar);
