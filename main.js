f10c0a3a1048980b8d12ef7a206c5a6d6cb79532
AndreLau11 <hectorlau.1993@gmail.com>
2022-03-01 18:49:28 -0600

    Add the objects elements

--- main.js
+++ main.js
@@ -21,40 +21,40 @@ let projects = [
     sourceLink: 'https://github.com/AndreLau11/Survey_Form'
   },
   second = {  
-    name: 'name',
-    description: 'description',
-    neature: 'neature',
+    name: 'Technical Documentation',
+    description: 'A description page of the new technology and how it impacts out complete daily living.',
+    feature: 'A fixed left panel that uses the complete screen hieght and the rest of the screen is for the documentation and detail information',
     image: 'image',
     technologies: 'technologies',
-    liveVersionLink: 'liveVersionLink',
-    sourceLink: 'sourceLink'
+    liveVersionLink: 'https://andrelau11.github.io/Technical_Documentation/',
+    sourceLink: 'https://github.com/AndreLau11/Technical_Documentation/settings/pages'
   },
   third = {  
-    name: 'name',
-    nescription: 'description',
-    neature: 'neature',
+    name: 'survey',
+    Description: 'A basic survey that shows all the posibilities on a survery using text filters, text input types, email and phone number validation and takes all that information and sent it to the destination.',
+    Feature: 'A page that uses all the inpt types, and filters for them ina good shape and send results.',
     image: 'image',
-    technologies: 'technologies',
-    liveVersionLink: 'liveVersionLink',
-    sourceLink: 'sourceLink'
+    technologies: 'HTML and inline CSS',
+    liveVersionLink: 'https://andrelau11.github.io/Survey/',
+    sourceLink: 'https://github.com/AndreLau11/Survey'
   },
   fourth = {  
-    name: 'name',
-    nescription: 'description',
-    neature: 'neature',
+    name: 'Personal Portfolio',
+    description: 'A general skill termination that includes all I have been capable of doing.',
+    feature: 'A basic website',
     image: 'image',
-    technologies: 'technologies',
-    liveVersionLink: 'liveVersionLink',
-    sourceLink: 'sourceLink'
+    technologies: 'HTML and inline CSS',
+    liveVersionLink: 'https://andrelau11.github.io/Personal_Portfolio/',
+    sourceLink: 'https://github.com/AndreLau11/Personal_Portfolio'
   },
   fifth = {  
-    name: 'name',
-    nescription: 'description',
-    neature: 'neature',
+    name: 'My Product',
+    description: 'A website that gives out and promotes the product to get it sell on the market.',
+    feature: 'A publicitary page that shows the best sides of the product',
     image: 'image',
-    technologies: 'technologies',
-    liveVersionLink: 'liveVersionLink',
-    sourceLink: 'sourceLink'
+    technologies: 'HTML and inline CSS',
+    liveVersionLink: 'https://andrelau11.github.io/My_Product/',
+    sourceLink: 'https://github.com/AndreLau11/My_Product'
   }
 ]
 
