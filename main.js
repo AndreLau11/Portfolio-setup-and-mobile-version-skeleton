4cf381d45c72d3f12cbbd775ba1e2423ceb17fab
AndreLau11 <hectorlau.1993@gmail.com>
2022-03-03 09:50:42 -0600

    Final details of the project

--- main.js
+++ main.js
@@ -5,18 +5,20 @@ const navPortfolio = document.getElementById('mobile__nav_portfolio');
 const navAbout = document.getElementById('mobile__nav_about');
 const navContact = document.getElementById('mobile__nav_contact');
 const cardContainer = document.getElementById('card-container');
-const buttons = cardContainer.getElementsByTagName('button');
+const portFolio = document.getElementById('projects-container');
+const buttons = portFolio.getElementsByTagName('button');
 
 const popUp = document.getElementById('popUp-slice');
-// const projectButton = document.getElementsByClassName('projectbutton');
-// const popUpImg = document.querySelector('.pop-img');
-// const popUpTitle = document.querySelector('.pop-title');
-// const popUpParagraph = document.querySelector('.pop-paragraph');
-// const buttonContainer = document.getElementById('projectDetails-popUp');
+const projectButton = document.getElementsByClassName('projectbutton');
+const popUpImg = document.querySelector('.pop-img');
+const popUpTitle = document.querySelector('.pop-title');
+const popUpParagraph = document.querySelector('.pop-paragraph');
+
+const buttonContainer = document.getElementById('projectDetails-popUp');
 
 const projectInformation = [
   {
-    id: 0,
+    id: 1,
     name: '',
     description: '',
     feature: '',
@@ -26,7 +28,7 @@ const projectInformation = [
     sourceLink: '',
   },
   {
-    id: 1,
+    id: 2,
     name: 'Survey Form',
     description:
       'This is a general survey form that can be used for your project at any  option you might have.',
@@ -38,7 +40,7 @@ const projectInformation = [
     sourceLink: 'https://github.com/AndreLau11/Survey_Form',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Technical Documentation',
     description:
       'A description page of the new technology and how it impacts out complete daily living.',
@@ -48,10 +50,10 @@ const projectInformation = [
     technologies: ['HTML', 'inline CSS'],
     liveVersionLink: 'https://andrelau11.github.io/Technical_Documentation/',
     sourceLink:
-      'https://github.com/AndreLau11/Technical_Documentation/settings/pages',
+      'https://github.com/AndreLau11/Technical_Documentation',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Survey',
     description:
       'A basic survey that shows all the posibilities on a survery using text filters, text input types, email and phone number validation and takes all that information and sent it to the destination.',
@@ -63,7 +65,7 @@ const projectInformation = [
     sourceLink: 'https://github.com/AndreLau11/Survey',
   },
   {
-    id: 4,
+    id: 5,
     name: 'Personal Portfolio',
     description:
       'A general skill termination that includes all I have been capable of doing.',
@@ -74,7 +76,7 @@ const projectInformation = [
     sourceLink: 'https://github.com/AndreLau11/Personal_Portfolio',
   },
   {
-    id: 5,
+    id: 6,
     name: 'My Product',
     description:
       'A website that gives out and promotes the product to get it sell on the market.',
@@ -88,18 +90,23 @@ const projectInformation = [
 
 function LoadProjects() {
   let counter;
-  // for (counter = 0; counter < projectInformation.length; counter += 1) {
-  //   let k;
-  //   let technologies = '';
-  //   for (k = 0; k < projectInformation[counter].technologies.length; k += 1) {
-  //     technologies += `<li>
-  //       <a href='#'>${projectInformation[counter].technologies[k]}</a>
-  //     </li>`;
-  //   }
+  for (counter = 0; counter < projectInformation.length; counter += 1) {
+    // if (counter == 0) {
+    //   projectInformation[counter].id = 1;
+    // }
+    let k;
+    let technologies = '';
+    for (k = 0; k < projectInformation[counter].technologies.length; k += 1) {
+      technologies += `<li>
+        <a href='#'>${projectInformation[counter].technologies[k]}</a>
+      </li>`;
+    }
 
     cardContainer.innerHTML += `<div class="projectsection${[counter]}">
         <h2 class="projecttitle">${projectInformation[counter].name}</h2>
-        <p class="projectparagraph">${projectInformation[counter].description}</p>
+        <p class="projectparagraph">${
+          projectInformation[counter].description
+        }</p>
         <ul class="skill">
             <li><a href=""><img class="skill-image-tag" src="./images/project/Tag0.png"
                         alt="Rubi legend, as reference of the back end language used on the project"></a>
@@ -114,61 +121,55 @@ function LoadProjects() {
   }
 
   function showModal(button) {
-    const pId = button.target.getAttribute('data-target');
-    const project = projectInformation.filter((p) => p.id === pId);
-    
-    popUp.innerHTML = ` <div class="projectdetails">
-          <div class="projectdetails-title">
-              <h3 id="popUpTitle" class="pop-title">${project[0].name}</h3>
-              <p id="popUpClose"><img id="popUpCloseImage" src="./images/icn-close.svg" alt="Exit Icon"></p>
-          </div>
-          <div>
-              <ul class="projectdetails-skills">
-                  <li><a href="#"><img src="./images/popup/Tag0.svg" alt="HTLM icon that leads to the html code"></a>
-                  </li>
-                  <li><a href="#"><img src="./images/popup/Tag1.svg"
-                              alt="Bootstrap icon that leads to the bootstrap format used on the project"></a></li>
-                  <li><a href="#"><img src="./images/popup/Tag2.svg"
-                              alt="Ruby on Rails icon that leads to the code on this lenguage that was used on the project"></a>
-                  </li>
-              </ul>
-          </div>
+    let pId = button.target.getAttribute('data-target');
+    let project = projectInformation.filter((p) => p.id == pId);
 
-          <div class="projectdetails-content">
-              <div class="projectdetails-image">
-                  <img class="pop-img" src="${project[0].image}"
-                      alt="A half scrren image that shows the first impresion of the results after the coding implementation">
-              </div>
-              <div class="projectdetails-body">
-                  <p class="pop-paragraph">${project[0].description}</p>
-                  <ul class="projectdetails-buttons">
-                      <li><button class="popUpSource" id="projectDetailButtonLive" onclick="location.href= ${project[0].liveVersionLink};">See Live <img src="./images/popup/Icon-see-live.svg" alt=""></button></li>
-                      <li><button class="popUpGithub" id="projectDetailButtonSource" onclick="location.href=${project[0].sourceLink};">See Source <img src="./images/popup/Vector.svg" alt=""></button></li>
-                  </ul>
-              </div>
-          </div>
-      </div>`;
+    popUp.innerHTML = `<div class="projectdetails">
+            <div class="projectdetails-title">
+                <h3 id="popUpTitle" class="pop-title">${project[0].name}</h3>
+                <p id="popUpClose"><img id="popUpCloseImage" src="./images/icn-close.svg" alt="Exit Icon"></p>
+            </div>
+            <div>
+                <ul class="projectdetails-skills">
+                    <li><a href="#"><img src="./images/popup/Tag0.svg" alt="HTLM icon that leads to the html code"></a>
+                    </li>
+                    <li><a href="#"><img src="./images/popup/Tag1.svg"
+                                alt="Bootstrap icon that leads to the bootstrap format used on the project"></a></li>
+                    <li><a href="#"><img src="./images/popup/Tag2.svg"
+                                alt="Ruby on Rails icon that leads to the code on this lenguage that was used on the project"></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="projectdetails-content">
+                <div class="projectdetails-image">
+                    <img class="pop-img" src="${project[0].image}"
+                        alt="A half scrren image that shows the first impresion of the results after the coding implementation">
+                </div>
+                <div class="projectdetails-body">
+                    <p class="pop-paragraph">${project[0].description}</p>
+                    <p class="anchorarea">  
+                      <a class="anchorbtn" id="projectDetailButtonLive" href = ${project[0].liveVersionLink} target="_blank">See Live <img src="./images/popup/Icon-see-live.svg" alt=""></a>
+                      <a class="anchorbtn" id="projectDetailButtonSource" href =${project[0].sourceLink} target="_blank">See Source <img src="./images/popup/Vector.svg" alt=""></a>
+                    </p>
+                </div>
+            </div>
+        </div>`;
     popUp.classList.remove('hidden');
     const popUpClose = document.getElementById('popUpClose');
-    function hideModal() {
-      popUp.innerHTML = '';
-      popUp.classList.add('hidden');
-    }
-
     popUpClose.addEventListener('click', hideModal);
   }
 
-  // function hideModal() {
-  //   popUp.innerHTML = '';
-  //   popUp.classList.add('hidden');
-  // }
+  function hideModal() {
+    popUp.innerHTML = '';
+    popUp.classList.add('hidden');
+  }
 
   let i;
   for (i = 0; i < buttons.length; i += 1) {
     buttons[i].addEventListener('click', showModal.bind(buttons[i]));
   }
 
-  // HAMBURGER MENU//
+  //HAMBURGER MENU//
 
   function showNavbar() {
     mobileNav.classList.remove('hidden');
@@ -180,16 +181,26 @@ function LoadProjects() {
     navOpen.classList.remove('hidden');
   }
 
-  // function showPopUp() {
-  //   popUp.classList.remove('hidden');
-    // }
-  //   function hidePopUp() {
-  //     popUp.classList.add('hidden');
-  //   }
-    navOpen.addEventListener('click', showNavbar);
-    navClose.addEventListener('click', hideNavbar);
-    navPortfolio.addEventListener('click', hideNavbar);
-    navAbout.addEventListener('click', hideNavbar);
-    navContact.addEventListener('click', hideNavbar);
+  navOpen.addEventListener('click', showNavbar);
+  navClose.addEventListener('click', hideNavbar);
+  navPortfolio.addEventListener('click', hideNavbar);
+  navAbout.addEventListener('click', hideNavbar);
+  navContact.addEventListener('click', hideNavbar);
+  
+
+  const storybutton = document.getElementById('storybottonAction');
+  const storypopup = document.getElementById('popUp-slicestory');
+  const storyClose = document.getElementById('popUpClosehistory')
+  
+  function showPopUp() {
+    storypopup.classList.remove('hidden');
+  }
+  function hidePopUp() {
+    storypopup.classList.add('hidden');
+  }
+  
+  storybutton.addEventListener('click', showPopUp);
+  storyClose.addEventListener('click', hidePopUp);
+}
 
 document.addEventListener('DOMContentLoaded', LoadProjects, false);
